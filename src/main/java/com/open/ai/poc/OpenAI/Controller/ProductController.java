9f0a8158cd341284f894339a23ee1d566b65ac04
Anshima20 <132668377+Anshima20@users.noreply.github.com>
2023-05-05 20:21:53 +0530

    Initial commit

---
+++ src/main/java/com/open/ai/poc/OpenAI/Controller/ProductController.java
@@ -0,0 +1,48 @@
+package com.open.ai.poc.OpenAI.Controller;
+
+import com.open.ai.poc.OpenAI.model.Product;
+import com.open.ai.poc.OpenAI.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService service;
+
+    @PostMapping
+    public Product addProduct(@RequestBody Product product) {
+        return service.saveProduct(product);
+    }
+
+    @GetMapping
+    public List<Product> findAllProducts() {
+        return service.getProducts();
+    }
+
+    @GetMapping("{id}")
+    public Product findProductById(@PathVariable int id) {
+        return service.getProductById(id);
+    }
+
+    @PutMapping
+    public Product updateProduct(@RequestBody Product product) {
+        return service.updateProduct(product);
+    }
+
+    @DeleteMapping("{id}")
+    public String deleteProduct(@PathVariable int id) {
+        return service.deleteProduct(id);
+    }
+}
